d96f63a02adbf2f63da314ba29405e60ca49cfb5
Ai-resume <patil>
2026-03-11 11:33:34 +0530

    fix vite-plugin-eslint types issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import eslint from 'vite-plugin-eslint';
+//import eslint from 'vite-plugin-eslint';
 import { defineConfig } from "vite"
 import tailwindcss from '@tailwindcss/vite'
+// @ts-expect-error vite-plugin-eslint does not have TypeScript type definitions
+import eslint from "vite-plugin-eslint"
 
  
 export default defineConfig({
